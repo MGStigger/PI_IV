b8ae4d38c704bd8275bfffa400654fc56f507f1f
ucpel202d5 <teste@gmail.com>
2019-09-04 20:30:47 -0300

    Testando o Laço WHILE

---
+++ prog2.py
@@ -0,0 +1,13 @@
+#exemplo de uso do break
+
+a = 0
+#while 1 == 1:
+while a < 5:
+
+	print(a)
+#	if a > 3:
+#		break
+	a += 1
+else:
+	print("Resultado do teste deu falso")
+print("fim do programa")
